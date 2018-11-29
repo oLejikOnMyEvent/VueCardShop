6afe415814d77761a58d6a47b8298685afe403b1
oLejikOnMyEvent <g3tf1ght@gmail.com>
2018-11-30 00:03:35 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import BuyModalComponent from '@/components/Shared/BuyModal'
+import * as fb from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
+import './styleus/main.styl'
+
+Vue.use(Vuetify)
+Vue.component('app-buy-modal', BuyModalComponent)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyAuDikCSI6t-yBL6VEW7m4DRnwsm1DxU1A',
+      authDomain: 'gorizont33-52047.firebaseapp.com',
+      databaseURL: 'https://gorizont33-52047.firebaseio.com',
+      projectId: 'gorizont33-52047',
+      storageBucket: 'gorizont33-52047.appspot.com',
+      messagingSenderId: '364429349880'
+    })
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
+
+    this.$store.dispatch('fetchAds')
+  }
+})
